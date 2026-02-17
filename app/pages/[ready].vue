0ee12a63220af1f042dfed17ba5b1657eb6ab28d
ENOKI TOKI <tokisora123456@gmail.com>
2026-02-18 01:02:13 +0900

    init

---
+++ app/pages/[ready].vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  h1 このページは準備中です
+  v-btn(
+    @click="$router.back()"
+  ) 戻る
+  v-btn(
+    @click="$router.push('/')"
+  ) トップページ
+</template>
+
+<script lang="ts">
+import mixins from '@/mixins/mixins'
+import Setup from '@/js/setup'
+
+export default {
+  mixins: [mixins],
+  setup() {
+    //サーバーサイドで仮のタイトルを設定、mountedで言語ごとに再設定する
+    Setup.setTitle('準備中')
+    Setup.setDescription('このページは現在準備中です。しばらくお待ちください。')
+  },
+  mounted() {
+    this.setTitle('準備中')
+  },
+}
+</script>
